74ef31e9244f399ba094461e1c3dde569fc1b926
dmitriypur <vst-pcmusic@yandex.ru>
2022-08-15 13:34:48 +0300

    sidebar

---
+++ src/js/files/sticky-block.js
@@ -0,0 +1,8 @@
+import stickyBlock from 'sticky-sidebar';
+
+var sidebar = new StickySidebar('#sidebar', {
+   containerSelector: '.page__wrap',
+   innerWrapperSelector: '.sidebar__wrap',
+   topSpacing: 20,
+   bottomSpacing: 20
+});
